3babbbc7648a1e6b97945a24e36d2eb5d0bbaa6f
MiaeJe <galduck2@daum.net>
2021-11-09 14:56:28 +0900

    문자열

--- 변수와문자열.py
+++ 변수와문자열.py
@@ -1,3 +1,4 @@
+'''
 # 여러개의 변수를 동시에 초기화
 # a, b, c = 1, 2, 3
 # print(a)
@@ -24,3 +25,41 @@ b= input("b: ")
 a, b = b, a
 print("a: " + a)
 print("b: " + b)
+
+'''
+
+
+# 문자열
+# 긴 문자열은 따옴표 3개(''', """)
+var3 = '''
+'따옴표' 3개는
+끝나는 "문장:까지 모두를 문자열로 처리
+---------------------!
+'''
+
+print(var3)
+
+'''
+# 문자열(+) 연산
+성='홍'
+이름='길동'
+name = 성+ ' ' + 이름
+
+print(name)
+
+# 타입변환 : str(), int(), float()
+
+print(type(int(str(100))))
+
+'''
+# 예제1 
+str1 = '\tIt\'s a "kind of" sunny\n Have a nice Day!'
+print(str1)
+
+# 예제2
+str2 = ''' 다스베이더가 말했다 \n "내가 니 애비다" \n 그 말을 들은 루크는 '깜짝'놀랐다. '''
+print(str2)
+
+city=input('태어난 도시가 어디인가요 ? \n')
+petName = input('애완 동물의 이름은? \n')
+print('당신의 밴드이름은 ' + city + ' ' + petName)
